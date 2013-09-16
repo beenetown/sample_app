0432659ab0f87378d20d915a0d467347cf0bf46f
John Beene <beenetown@yahoo.com>
2013-09-16 12:52:46 -0400

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name                  "John Beene"
-    email                 "john@example.com"
-    password              "foobar"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@railstutorial.org" }
+    password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
